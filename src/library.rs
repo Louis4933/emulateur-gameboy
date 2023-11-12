c3fa70837bb78fd1c66e4fe81ba787ce97f4558a
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2023-11-12 17:13:26 +0100

    main + lib need fix

--- src/library.rs
+++ src/library.rs
@@ -1,5 +1,15 @@
+mod cartouches;
+mod cpu;
 mod joypad;
+mod memoire;
+mod mmu;
+mod ppu;
+mod timer;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::memoire::Memoire;
 #[derive(Clone, Copy)]
 pub enum GameboyButton {
     Right,
@@ -27,8 +37,52 @@ impl From<GameboyButton> for joypad::JoypadKey {
     }
 }
 
+pub struct Gameboy {
+    mmu: Rc<RefCell<mmu::Mmu>>,
+    cpu: cpu::RealTimeCpu,
+}
+
 impl Gameboy {
 
-    
+    pub fn new(rom: Vec<u8>) -> Gameboy {
+        let cartouche = cartouches::new(rom);
+        let mmu = Rc::new(RefCell::new(mmu::Mmu::new(cartouche)));
+        let cpu = cpu::RealTimeCpu::new(mmu.clone());
+        Gameboy { mmu, cpu }
+    }
 
+    pub fn step(&mut self) -> u32 {
+        if self.mmu.borrow().get_octet(self.cpu.cpu.registres.pc) == 0x10 {
+            self.mmu.borrow_mut().perform_vitesse_switch();
+        }
+        let cycles = self.cpu.run();
+        self.mmu.borrow_mut().run_cycles(cycles);
+        cycles
+    }
+
+    pub fn has_screen_updated(&mut self) -> bool {
+        let result = self.mmu.borrow().ppu.vblank;
+        self.mmu.borrow_mut().ppu.vblank = false;
+        result
+    }
+
+    pub fn get_screen_dimension(&self) -> [usize;2] {
+        return [ppu::SCREEN_HEIGHT, ppu::SCREEN_WIDTH];
+    }
+    
+    pub fn get_screen_data(&self) -> [ppu::Pixel; ppu::SCREEN_WIDTH * ppu::SCREEN_HEIGHT] {
+        self.mmu.borrow().ppu.data
+    }
+
+    pub fn can_take_input(&mut self) -> bool {
+        self.cpu.flip()
+    }
+
+    pub fn gerer_keyup(&mut self, button: GameboyButton) {
+        self.mmu.borrow_mut().joypad.keyup(button.into());
+    }
+
+    pub fn gerer_keydown(&mut self, button: GameboyButton) {
+        self.mmu.borrow_mut().joypad.keydown(button.into());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+use crate::library::GameboyButton;
+use crate::library::Gameboy;
+use std::fs::File;
+use std::io::Read;
+
+use argparse::{ArgumentParser, Store};
+use minifb::{Key, Scale, Window, WindowOptions};
+
+const KEY_MAPPINGS: [(Key, GameboyButton); 8] = [
+    (Key::Right, GameboyButton::Right),
+    (Key::Up, GameboyButton::Up),
+    (Key::Left, GameboyButton::Left),
+    (Key::Down, GameboyButton::Down),
+    (Key::Z, GameboyButton::A),
+    (Key::X, GameboyButton::B),
+    (Key::Space, GameboyButton::Select),
+    (Key::Enter, GameboyButton::Start),
+];
+
 fn main() {
-    println!("Hello, world!");
+    let mut rom_path = String::from("");
+    {
+        let mut arg_parser = ArgumentParser::new();
+        arg_parser.set_description("Emulateur de Gameboy");
+        arg_parser
+            .refer(&mut rom_path)
+            .add_argument("rom", Store, "Chemin")
+            .required();
+        arg_parser.parse_args_or_exit();
+    }
+
+    let window_options = WindowOptions {
+        resize: true,
+        scale: Scale::X2,
+        ..Default::default()
+    };
+
+    let mut file = File::open(rom_path.clone()).unwrap();
+    let mut rom = Vec::new();
+    file.read_to_end(&mut rom).unwrap();
+
+    let mut gameboy = Gameboy::new(rom);
+
+    let mut window = Window::new(
+        &format!("Gameboy"),
+        gameboy.get_screen_dimension()[1],
+        gameboy.get_screen_dimension()[0],
+        window_options,
+    )
+    .unwrap();
+    let mut window_buffer = vec![0x00; gameboy.get_screen_dimension()[0] * gameboy.get_screen_dimension()[1]];
+    window
+        .update_with_buffer(window_buffer.as_slice(), gameboy.get_screen_dimension()[1], gameboy.get_screen_dimension()[0])
+        .unwrap();
+
+    while window.is_open() {
+        gameboy.step();
+        if gameboy.has_screen_updated() {
+            for (i, pixel) in gameboy.get_screen_data().iter().enumerate() {
+                let r = u32::from(pixel.r) << 16;
+                let g = u32::from(pixel.g) << 8;
+                let b = u32::from(pixel.b);
+                let a = 0xFF00_0000;
+                window_buffer[i] = a | r | g | b;
+            }
+            window
+                .update_with_buffer(window_buffer.as_slice(), gameboy.get_screen_dimension()[1], gameboy.get_screen_dimension()[0])
+                .unwrap();
+        }
+        if gameboy.can_take_input() {
+            for (physical_key, gameboy_button) in &KEY_MAPPINGS {
+                if window.is_key_down(*physical_key) {
+                    gameboy.gerer_keydown(*gameboy_button);
+                } else {
+                    gameboy.gerer_keyup(*gameboy_button);
+                }
+            }
+        }
+    }
 }
